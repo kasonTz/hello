11fcf506aec20d7ebd5da9a3e3154ed2113f98e0
kai <924456295@qq.com>
2019-03-20 15:18:32 +0800

    添加了 HomeController

--- app/src/main/java/com/newer/AppApplication.java
+++ app/src/main/java/com/newer/AppApplication.java
@@ -3,6 +3,11 @@ package com.newer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author kai
+ *
+ */
 @SpringBootApplication
 public class AppApplication {
 
